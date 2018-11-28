4e8c19e0d4bf94deac74716841980a62f32fb3ea
Vinnie Ramirez <vinnie.ramirez@gmail.com>
2018-11-28 07:48:24 -0600

    priv mod reg test

--- main.tf
+++ main.tf
@@ -5,7 +5,11 @@ module "vpc" {
   version = "1.44.0"
 
   cidr = "10.0.0.0/16"
+  azs = ["us-east-2a", "us-east-2b"]
+  private_subnets = ["10.0.0.1/24", "10.0.0.2/24"]
   create_vpc = "true"
+  enable_nat_gateway = "true"
+  enable_vpn_gateway = "true"
   default_vpc_enable_dns_hostnames = "true"
   default_vpc_name = "dev001"
   name = "dev001-vpc"
